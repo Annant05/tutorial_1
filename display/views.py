69f9bc1f171f0eb427b80b3b79d2e39bc0cfa052
Raghvi <gupta.raghvi12@gmail.com>
2021-01-05 17:48:56 +0530

    Display module completed

--- display/views.py
+++ display/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+import json
 # Create your views here.
+
+def index(request):
+    f = open("./db/student_list.json", "r")
+    student_list = json.loads(f.read())["stud"]
+    print({"student_list":student_list})
+
+    return render(request, 'display/table.html ', {"student_list":student_list})
